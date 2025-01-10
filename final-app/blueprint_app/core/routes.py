fefb0e5eab2c197413dd62c9ad51ae7017c97f69
LIJO JOSEPH <132869542+LIJO20041997@users.noreply.github.com>
2025-01-10 11:28:42 +0530

    Delete final-app directory

--- final-app/blueprint_app/core/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import request, render_template, Blueprint
-from blueprint_app.app import db
-
-core = Blueprint('core',__name__,   template_folder='templates' )
-
-@core.route('/')
-def index():
-    return render_template('core/index.html')
